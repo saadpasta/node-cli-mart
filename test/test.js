ee59cd594ff65d4add1676d3e2cbeaae9922aeb1
Saad Pasta <saadpasta70@gmail.com>
2020-12-16 16:16:44 +0500

    Write Test For Adding and Bill

--- test/test.js
+++ test/test.js
@@ -5,3 +5,27 @@ test('Intial Testing', async (done) => {
   expect(result).toBe('Testing');
   done();
 });
+
+test('Checkout with empty cart', async (done) => {
+  const result = commands.parseInput('checkout');
+  expect(result).toBe('empty cart');
+  done();
+});
+
+test('Add 5 Soap', async (done) => {
+  const result = commands.parseInput('add soap 5');
+  expect(result).toBe('added soap 5');
+  done();
+});
+
+test('Add 1 Bread', async (done) => {
+  const result = commands.parseInput('add bread 1');
+  expect(result).toBe('added bread 1');
+  done();
+});
+
+test('Bill', async (done) => {
+  const result = commands.parseInput('bill');
+  expect(result).toBe('subtotal:52.50, discount:0.00, total:52.50');
+  done();
+});
