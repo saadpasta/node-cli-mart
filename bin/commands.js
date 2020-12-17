5729fa68924a31ee0f7ba7ed8d13f064d2e6e248
Saad Pasta <saadpasta70@gmail.com>
2020-12-17 14:05:36 +0500

    Add : Comment and checkout command

--- bin/commands.js
+++ bin/commands.js
@@ -1,5 +1,19 @@
-let { inventory } = require('./inventory');
-let { addProductToCart, cart, printBill,addOfferOnProduct } = require('./app');
+/**
+ *
+ * @param {string} params Complete user input from cli
+ * @param {string} params[0] Input command like (add) , (offer) ,(bill) and (checkout) from which we decide how to procced
+ * @param {string} params[1] Name of product or offer.
+ * @param {string} params[2] Number of product if command is (add) and name of product if command is (offer)
+ *
+ */
+
+let {
+  addProductToCart,
+  cart,
+  printBill,
+  addOfferOnProduct,
+  checkout,
+} = require('./app');
 
 const parseInput = (params) => {
   const userCommand = params.split(' ');
@@ -12,15 +26,14 @@ const checkInput = (params) => {
   switch (input) {
     case 'add':
       return addProductToCart(params[1], params[2]);
-      break;
     case 'offer':
       return addOfferOnProduct(params[1], params[2]);
-      break;
     case 'bill':
       return printBill();
-      break;
+    case 'checkout':
+      return checkout();
     default:
-      return null;
+      return 'No such command found';
   }
 };
 
