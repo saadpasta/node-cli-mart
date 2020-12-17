d4977a9654353d70a0d33ea7e8c3e5b8b9c8f837
Saad Pasta <saadpasta70@gmail.com>
2020-12-17 16:23:47 +0500

    read : filename from argumens

--- bin/supermarket.js
+++ bin/supermarket.js
@@ -1,7 +1,10 @@
 #!/usr/bin/env node
 
 const readline = require('readline');
-var commands = require('./commands');
+const fs = require('fs');
+const commands = require('./commands');
+
+const { readInventoryFile } = require('./inventory');
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -9,12 +12,28 @@ const rl = readline.createInterface({
   prompt: '$ ',
 });
 
-rl.prompt();
-rl.on('line', (line) => {
-  const result = commands.parseInput(line.trim());
-  console.log(result);
+const args = process.argv.slice(2);
+
+const inventoryFileName = args[0];
+
+readInventoryFile(inventoryFileName).then(() => {
   rl.prompt();
-}).on('close', () => {
-  console.log('Have a great day!');
-  process.exit(0);
+  rl.on('line', (line) => {
+    const result = commands.parseInput(line.trim());
+    console.log(result);
+
+    // exit cli if command is checkout
+    if (result === 'done') {
+      process.exit(0);
+    }
+
+    rl.prompt();
+  }).on('close', () => {
+    console.log('Have a great day!');
+    process.exit(0);
+  });
 });
+
+module.exports = {
+  inventoryFileName,
+};
