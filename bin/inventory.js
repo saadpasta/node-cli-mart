003f4b3eb7dc02656a7f6cbfbcd1dce1a25e8677
Saad Pasta <saadpasta70@gmail.com>
2020-12-17 17:08:05 +0500

    Feat : Functionality to run commands from text file

--- bin/inventory.js
+++ bin/inventory.js
@@ -1,3 +1,4 @@
+const { rejects } = require('assert');
 const fs = require('fs');
 const readline = require('readline');
 
@@ -5,19 +6,22 @@ const inventory = {};
 
 const readInventoryFile = (filename) => {
   return new Promise(function (resolve, reject) {
-    const readInterface = readline.createInterface({
-      input: fs.createReadStream(filename),
-      console: false,
-    });
-
-    readInterface.on('line', function (line) {
-      const product = line.split(',');
-      inventory[product[0]] = {
-        amount: product[1],
-        quantity: product[2],
-      };
-      resolve(true)
-    });
+    try {
+      const readInterface = readline.createInterface({
+        input: fs.createReadStream(filename),
+        console: false,
+      });
+      readInterface.on('line', function (line) {
+        const product = line.split(',');
+        inventory[product[0]] = {
+          amount: product[1],
+          quantity: product[2],
+        };
+        resolve(true);
+      });
+    } catch {
+      reject('wrong filename');
+    }
   });
 };
 
--- bin/supermarket.js
+++ bin/supermarket.js
@@ -15,24 +15,55 @@ const rl = readline.createInterface({
 const args = process.argv.slice(2);
 
 const inventoryFileName = args[0];
+const commandsFileName = args[1];
 
-readInventoryFile(inventoryFileName).then(() => {
-  rl.prompt();
-  rl.on('line', (line) => {
-    const result = commands.parseInput(line.trim());
-    console.log(result);
-
-    // exit cli if command is checkout
-    if (result === 'done') {
+// filename is provided then run the interative mode
+if (inventoryFileName) {
+  readInventoryFile(inventoryFileName)
+    .then(() => {
+      if (commandsFileName) {
+        // command filename is  provided then run automated commad from textfile
+        runAutomatedCommands();
+      } else {
+        rl.prompt();
+        rl.on('line', (line) => {
+          printResult(line);
+          rl.prompt();
+        }).on('close', () => {
+          console.log('Have a great day!');
+          process.exit(0);
+        });
+      }
+    })
+    .catch((error) => {
+      console.log(error);
       process.exit(0);
-    }
+    });
+} else {
+  console.log('Please provide a filename');
+  process.exit(0);
+}
 
-    rl.prompt();
-  }).on('close', () => {
-    console.log('Have a great day!');
-    process.exit(0);
+function runAutomatedCommands() {
+  const readInterface = readline.createInterface({
+    input: fs.createReadStream(commandsFileName),
+    console: false,
   });
-});
+  readInterface.on('line', function (line) {
+    console.log(`$ ${line}`);
+    printResult(line);
+  });
+}
+
+function printResult(line) {
+  const result = commands.parseInput(line.trim());
+  console.log(result);
+
+  // exit cli if command is checkout
+  if (result === 'done') {
+    process.exit(0);
+  }
+}
 
 module.exports = {
   inventoryFileName,
